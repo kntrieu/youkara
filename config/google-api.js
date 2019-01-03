48eb567f1019cefb5fd6dade9f4a74777690baa6
Trieu Kiem-Nhut <30973656+kntrieu@users.noreply.github.com>
2019-01-03 14:53:21 +0700

    Update google-api.js

--- config/google-api.js
+++ config/google-api.js
@@ -1,5 +1,1 @@
-module.exports = Object.freeze({
-    API_KEY_1: "AIzaSyBmYfgTYJ1e2IJD-ZQTWSDcb1sKUwIs3Mk",
-    CLIENT_ID: "1087303386594-d2kio0qsun6vbm98ob6va7fmnjbcil2e.apps.googleusercontent.com",
-    CLIENT_SECRET: "8FrTd2PynyTOwVx2HXafpwx2"
-});
+Hide for private issue

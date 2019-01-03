b58b8dca1fdc4b7874deda7a63c8b05288e25642
Trieu Kiem-Nhut <30973656+kntrieu@users.noreply.github.com>
2019-01-03 14:52:18 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 //This is the secret database information
 module.exports = Object.freeze({
-    CONECTION_STRING: "mongodb://kntrieu:DrugsashiechFa5@ds141932.mlab.com:41932/youkara",
-    SECRET: "DrugsashiechFa5"
+    CONECTION_STRING: "XXXXXXXX",
+    SECRET: "DXXXXXXX"
 });
